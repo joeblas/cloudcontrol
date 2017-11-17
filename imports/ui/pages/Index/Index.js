8f046a262c33541ed2bcaa3206800394832f10bb
Joe Gallegos <joebgallegos@gmail.com>
2017-11-16 21:21:20 -0700

    Cleaned up UI

--- imports/ui/pages/Index/Index.js
+++ imports/ui/pages/Index/Index.js
@@ -9,15 +9,9 @@ const Index = () => (
       src="https://s3-us-west-2.amazonaws.com/cleverbeagle-assets/graphics/email-icon.png"
       alt="Clever Beagle"
     />
-    <h1>Pup</h1>
-    <p>A boilerplate for products.</p>
-    <div>
-      <Button href="http://cleverbeagle.com/pup">Read the Docs</Button>
-      <Button href="https://github.com/cleverbeagle/pup"><i className="fa fa-star" /> Star on GitHub</Button>
-    </div>
-    <footer>
-      <p>Need help and want to stay accountable building your product? <a href="http://cleverbeagle.com?utm_source=pupappindex&utm_campaign=oss">Check out Clever Beagle</a>.</p>
-    </footer>
+    <h1>CloudControl</h1>
+    <p>Simple Cloud Based Invoicing.</p>
+  
   </div>
 );
 
