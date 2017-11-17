65b8bf9acc3fa313fa33149108d3df7e052ce6ba
Ethan Zander <ethanzander13@gmail.com>
2017-11-17 00:35:33 -0700

    replaced 'application name' with 'controlcloud'

--- imports/ui/pages/Index/Index.js
+++ imports/ui/pages/Index/Index.js
@@ -5,10 +5,7 @@ import './Index.scss';
 
 const Index = () => (
   <div className="Index">
-    <img
-      src="https://s3-us-west-2.amazonaws.com/cleverbeagle-assets/graphics/email-icon.png"
-      alt="Clever Beagle"
-    />
+    <img src="./mainLogoLarge.jpg"/>
     <h1>CloudControl</h1>
     <p>Simple Cloud Based Invoicing.</p>
   
